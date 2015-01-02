35381e17ed2f458d107350b8cdcfa65e95b41c72
Jaymz Rhime <wxactly@gmail.com>
2015-01-01 23:06:32 -0800

    formatting

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,20 @@
-angular.module( 'scribbles', [
+angular.module('scribbles', [
   'templates-app',
   'templates-common',
   'scribbles.index',
   'scribbles.scribble',
   'ui.router'
 ])
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/' );
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
 })
-.run( function run () {
+.run(function() {
 	
 })
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+.controller('AppCtrl', function($scope, $location) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | scribbles - wxactly' ;
+    if(angular.isDefined(toState.data.pageTitle)) {
+      $scope.pageTitle = toState.data.pageTitle + ' | scribbles - wxactly';
     }
   });
 });
